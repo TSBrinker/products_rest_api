ba6af039f5508c0bc138eb8895d920ec1397853e
TSBrinker <tsbrinker@gmail.com>
2022-10-07 11:03:54 -0500

    Added product_image field to model and to serializer + updated ERD to reflect the product_image field.

--- products/models.py
+++ products/models.py
@@ -8,3 +8,4 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     inventory_quantity = models.IntegerField()
+    product_image = models.URLField(max_length=200, blank=True)
